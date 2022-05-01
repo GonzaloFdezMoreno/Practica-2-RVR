e3745ee2cda3ecda1aa82c2589632c6b66834cc8
pablos16 <pablos16@ucm.es>
2022-05-02 00:03:53 +0200

    terminada la practica aunque no hemos logrado que haga un unique pointer

--- Practica2.2/replicacion-chat/Chat.cc
+++ Practica2.2/replicacion-chat/Chat.cc
@@ -1,4 +1,5 @@
 #include "Chat.h"
+#include <memory.h>
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
@@ -10,6 +11,15 @@ void ChatMessage::to_bin()
     memset(_data, 0, MESSAGE_SIZE);
 
     //Serializar los campos type, nick y message en el buffer _data
+    char *temp = _data;
+
+    memcpy(temp, &type, sizeof(uint8_t));
+    temp += sizeof(uint8_t);
+
+    memcpy(temp, nick.c_str(), sizeof(char) * 8);
+    temp += sizeof(char) * 8;
+
+    memcpy(temp, message.c_str(), sizeof(char) * 80);
 }
 
 int ChatMessage::from_bin(char * bobj)
@@ -19,6 +29,15 @@ int ChatMessage::from_bin(char * bobj)
     memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
 
     //Reconstruir la clase usando el buffer _data
+    char *temp = _data;
+
+    memcpy(&type, temp, sizeof(uint8_t));
+    temp += sizeof(uint8_t);
+
+    nick = temp;
+    temp += sizeof(char) * 8;
+
+    message = temp;
 
     return 0;
 }
@@ -35,11 +54,50 @@ void ChatServer::do_messages()
          * crear un unique_ptr con el objeto socket recibido y usar std::move
          * para añadirlo al vector
          */
+        
+        ChatMessage chatmess;
+        Socket * sock;
+
+        socket.recv(chatmess, sock);
 
         //Recibir Mensajes en y en función del tipo de mensaje
         // - LOGIN: Añadir al vector clients
         // - LOGOUT: Eliminar del vector clients
         // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+        int counter = 0;
+        switch(chatmess.type) {
+            case ChatMessage::LOGIN:
+                std::cout << chatmess.nick << " connected" << std::endl;
+                //Este da problemas con el unique pointer porque std no tiene make_unique aqui y no logramos resolverlo
+                clients.push_back(std::move(std::unique_ptr<Socket>(sock)));
+            break;
+            case ChatMessage::LOGOUT:
+                auto it = clients.begin();
+                while(it != clients.end() && (**it != *sock)){
+                    it++;
+                }
+                if(it == clients.end()){
+                    std::cout << "The player had already disconnected" << std::endl;
+                }
+                else{
+                    std::cout << chatmess.nick << " disconnected" << std::endl;
+                    clients.erase(it);
+                    Socket *socktodel = (*it).release();
+                    delete socktodel;
+                }
+            break;
+            case ChatMessage::MESSAGE:
+                for(auto it = clients.begin(); it != clients.end(); it++){
+                    if(**it != *sock){
+                        socket.send(chatmess, **it);
+                    }
+                }
+            break;
+            default:
+                std::cout << "Error unknown message received" << std::endl;
+            break;
+        }
+
     }
 }
 
@@ -59,6 +117,12 @@ void ChatClient::login()
 void ChatClient::logout()
 {
     // Completar
+    std::string msg;
+
+    ChatMessage em(nick, msg);
+    em.type = ChatMessage::LOGOUT;
+
+    socket.send(em, socket);
 }
 
 void ChatClient::input_thread()
@@ -67,6 +131,15 @@ void ChatClient::input_thread()
     {
         // Leer stdin con std::getline
         // Enviar al servidor usando socket
+        std::string msg;
+        std::getline(std::cin, msg);
+
+        if (msg.size() > 80) msg.resize(80);
+
+        ChatMessage em(nick, msg);
+        em.type = ChatMessage::MESSAGE;
+
+        socket.send(em, socket);
     }
 }
 
@@ -76,6 +149,9 @@ void ChatClient::net_thread()
     {
         //Recibir Mensajes de red
         //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
+        ChatMessage em;
+        socket.recv(em);
+        std::cout << em.nick << ": " << em.message << std::endl;
     }
 }
 
--- Practica2.2/replicacion-chat/Socket.cc
+++ Practica2.2/replicacion-chat/Socket.cc
@@ -7,6 +7,27 @@ Socket::Socket(const char * address, const char * port):sd(-1)
 {
     //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
     //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    struct addrinfo hints;
+    struct addrinfo *result;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int rc = getaddrinfo(address, port, &hints, &result);
+    if(rc != 0){
+        std::cerr << "Error getaddrinfo: " << gai_strerror(rc) << std::endl;
+    }
+
+    sd = socket(result->ai_family, result->ai_socktype, 0);
+    if(sd == -1){
+        std::cerr << "Error socket: " << gai_strerror(sd) << std::endl;
+    }
+
+    sa = *result->ai_addr;
+    sa_len = result->ai_addrlen;
+
+    freeaddrinfo(result);
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
@@ -36,7 +57,17 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 int Socket::send(Serializable& obj, const Socket& sock)
 {
     //Serializar el objeto
+    obj.to_bin();
     //Enviar el objeto binario a sock usando el socket sd
+
+    ssize_t bytessend = sendto(sock.sd, obj.data(), obj.size(), 0, &sock.sa, sock.sa_len);
+
+    if(bytessend <= 0){
+        std::cerr << "Error del envio del mensaje" << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 bool operator== (const Socket &s1, const Socket &s2)
@@ -44,6 +75,22 @@ bool operator== (const Socket &s1, const Socket &s2)
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
+    
+    if(s1.sa.sa_family != s2.sa.sa_family){
+        return false;
+    }
+
+    struct sockaddr_in *sin1 = (struct sockaddr_in *) &(s1.sa);
+    struct sockaddr_in *sin2 = (struct sockaddr_in *) &(s2.sa);
+
+    if(sin1->sin_addr.s_addr != sin2->sin_addr.s_addr){
+        return false;
+    }
+    if(sin1->sin_port != sin2->sin_port){
+        return false;
+    }
+
+    return true;
 };
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
@@ -59,3 +106,7 @@ std::ostream& operator<<(std::ostream& os, const Socket& s)
     return os;
 };
 
+bool operator!= (const Socket &s1, const Socket &s2)
+{
+    return !(s1 == s2);
+};
